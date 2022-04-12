39773f30c560cc3e12303ddb96300cf508b93ebe
Velisarios1234 <fafoutis@csd.auth.gr>
2022-04-12 10:33:05 +0300

    project updated

---
+++ src/SideBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Sidebar.css'
+
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+//import { Button } from "@mui/core";
+
+export default function Sidebar() {
+    return(
+        <div className='sidebar'>
+            <TwitterIcon/>
+        </div>
+    )
+}
